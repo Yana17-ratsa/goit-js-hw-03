8f813d436c606a46b04fbfd0735fe19ff4acbece
Yana17-ratsa <ratsayana13@gmail.com>
2023-12-06 12:20:55 +0200

    task3 update

--- js/task-2.js
+++ js/task-2.js
@@ -2,7 +2,7 @@ function makeArray(firstArray, secondArray, maxLength) {
 
     const thirdArray = firstArray.concat(secondArray);
     if (thirdArray.length > maxLength) {
-            return thirdArray.slice(0, maxLength)
+        return thirdArray.slice(0, maxLength)
     } else {
         return thirdArray;
     }
